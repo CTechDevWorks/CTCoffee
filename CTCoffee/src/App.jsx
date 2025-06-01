94334305a8937a346c8f75cdb8b60ccbf8a23d48
TboyxfireXD <CTechDevWorks@gmail.com>
2025-06-01 22:28:17 +0200

    updated nav bar, completed hero section

--- CTCoffee/src/App.jsx
+++ CTCoffee/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
-import Hero from './components/Hero'
+import HomePage from './components/HomePage'
 import Shop from './components/Shop'
 import Reviews from './components/Reviews'
 import Contact from './components/Contact'
@@ -13,7 +13,7 @@ const App = () => {
       <Router>
         <Navs />
         <Routes>
-          <Route path='/' element={<Hero />} />
+          <Route path='/' element={<HomePage />} />
           <Route path='/shop' element={<Shop />} />
           <Route path='/reviews' element={<Reviews />}/>
           <Route path='/contact' element={<Contact />}/>
---
+++ CTCoffee/src/components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="hero">
+      <div className="header">
+        <h1>Welcome to Our Lovely Coffee store!!!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
